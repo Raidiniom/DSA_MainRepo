c4d5770bef8ee0959ccfc05b21791450516f9374
raidiniom <raidiniom22ph@gmail.com>
2025-09-16 16:21:10 +0800

    ADT List Structure with Different Versions

---
+++ ADTList/version1/adt.h
@@ -0,0 +1,123 @@
+#ifndef ADT_h
+#define ADT_h
+
+// Description here!
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define max_size 10
+
+typedef struct {
+    char name[24];
+    int stock;
+} Product;
+
+typedef struct {
+    Product prodList[max_size];
+    int count;
+} ADTList;
+
+// Helper Functions
+Product createProduct(char* name, int stock) {
+    Product initialize;
+
+    strcpy(initialize.name, name);
+    initialize.stock = stock;
+
+    return initialize;
+}
+
+void displayProduct(Product prod) {
+    printf("%-16s    %d\n", prod.name, prod.stock);
+}
+
+// Operations
+void initADTLIST(ADTList* list) {
+    list->count = 0;
+}
+
+bool isEmpty(ADTList list) {
+    return list.count == 0;
+}
+
+bool isFull(ADTList list) {
+    return list.count >= max_size;
+}
+
+void insertFirst(ADTList* list, Product prod) {
+    if (!isFull(*list))
+    {
+        list->count++;
+
+        for (int i = list->count - 1; i >= 0; i--)
+        {
+            list->prodList[i + 1] = list->prodList[i];
+        }
+        
+        list->prodList[0] = prod;
+    }
+    else
+    {
+        printf("List is full\n");
+    }
+    
+}
+
+void insertLast(ADTList* list, Product prod) {
+    if (!isFull(*list))
+    {
+        list->prodList[list->count++] = prod;
+    }
+    else
+    {
+        printf("List is full\n");
+    }
+    
+}
+
+void insertAt(ADTList* list, Product prod, int position) {
+    if (!isFull(*list))
+    {
+        list->count++;
+        int index = position - 1;
+        for (int i = list->count - 1; i > index; i--)
+        {
+            list->prodList[i] = list->prodList[i - 1];
+        }
+        
+        list->prodList[position - 1] = prod;
+    }
+    else
+    {
+        printf("List is full\n");
+    }
+    
+}
+
+void deleteFirst(ADTList* list) {
+    if (!isEmpty(*list))
+    {
+        for (int i = 0; i < list->count; i++)
+        {
+            list->prodList[i] = list->prodList[i + 1];
+        }
+        list->count--;
+    }
+    else
+    {
+        printf("List is Empty\n");
+    }
+    
+}
+
+void displayList(ADTList list) {
+    for (int i = 0; i < list.count; i++)
+    {
+        displayProduct(list.prodList[i]);
+    }
+    
+}
+
+#endif
---
+++ ADTList/version1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "adt.h"
+
+void main() {
+    ADTList myList;
+    initADTLIST(&myList);
+
+    insertFirst(&myList, createProduct("Camon 30", 50));
+    insertFirst(&myList, createProduct("Iphone 16", 100));
+    insertFirst(&myList, createProduct("Samsung Galaxy", 20));
+    insertLast(&myList, createProduct("Honor X8C", 15));
+    insertAt(&myList, createProduct("4Tech Mouse", 10), 2);
+
+    insertLast(&myList, createProduct("Logitech K380", 25));
+    insertFirst(&myList, createProduct("Macbook Air M3", 12));
+    insertLast(&myList, createProduct("Dell XPS 13", 8));
+    insertAt(&myList, createProduct("HP Envy x360", 18), 5);
+    insertLast(&myList, createProduct("ASUS ROG Phone", 7));
+
+    printf("Inserting Data:\n");
+    displayList(myList);
+
+    deleteFirst(&myList);
+
+    printf("\n\nDeleting Data:\n");
+    displayList(myList);
+}
