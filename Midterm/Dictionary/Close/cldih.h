8b0717c698524874c5616dc9f028b6d01dba0a76
Raidiniom <raidiniom22ph@gmail.com>
2025-10-30 01:30:22 +0800

    Tom is Exam

--- Midterm/Dictionary/Close/cldih.h
+++ Midterm/Dictionary/Close/cldih.h
@@ -12,48 +12,69 @@ typedef enum {
     OCCUPIED,
 } Status;
 
-typedef struct node {
+typedef struct {
     int element;
     Status status;
-    struct node* next;
-} *Node, SizeNode;
+} Entry;
 
-typedef Node Dictionary[max];
+typedef Entry Dictionary[max];
 
 int hashFunct(int element) {
     if (element < 0) element = -element;
     return element % max;
 }
 
-void initDictionary(Dictionary dih) {
+void initDictionary(Dictionary* dih) {
     for (int i = 0; i < max; i++)
     {
-        dih[i] = NULL;
+        (*dih)[i].element = 0;
+        (*dih)[i].status = EMPTY;
     }
     
 }
 
+void insertElem(Dictionary* dih, int element) {
+    int index = hashFunct(element);
+    int start = index;
+
+    do {
+        if ((*dih)[index].status == EMPTY || (*dih)[index].status == DELETE) {
+            (*dih)[index].element = element;
+            (*dih)[index].status = OCCUPIED;
+            break;
+        }
+        // Go to next index (wrap around with % max)
+        index = (index + 1) % max;
+    } while (index != start);
+}
+
+void deleteElem(Dictionary* dih, int element) {
+    int index = hashFunct(element);
+    int start = index;
+
+    do {
+        if ((*dih)[index].status == OCCUPIED && (*dih)[index].element == element) {
+            (*dih)[index].status = DELETE;
+            printf("Deleted %d from index %d\n", element, index);
+            return;
+        }
+        index = (index + 1) % max;
+    } while (index != start);
+
+    printf("%d not found in dictionary\n", element);
+}
+
 void display(Dictionary dih, char* label) {
-    printf("-----%24s------", label);
+    printf("-----%24s------\n", label);
 
     for (int i = 0; i < max; i++)
     {
         printf("%d | ", i);
-        if (dih[i] != NULL)
+        switch (dih[i].status)
         {
-            Node trav = dih[i];
-            
-            for (; trav != NULL; trav = trav->next)
-            {
-                printf("[ %d ]", trav->element);
-
-                if (trav->next) printf(" -> ");
-            }
-        }
-        else
-        {
-            printf("NULL");
-            
+            case EMPTY: printf("[ EMPTY ]"); break;
+            case DELETE: printf("[ DELETE ]"); break;
+            default: printf("%d", dih[i].element); break;
         }
         printf("\n");
     }
--- Midterm/Dictionary/Close/main.c
+++ Midterm/Dictionary/Close/main.c
@@ -1,4 +1,20 @@
+#include "cldih.h"
 
 void main() {
-    printf("Close Dictiionary");
+    Dictionary cld;
+    initDictionary(&cld);
+
+    insertElem(&cld, 1);
+    insertElem(&cld, 2);
+    insertElem(&cld, 3);
+    insertElem(&cld, 3);
+    insertElem(&cld, 4);
+    insertElem(&cld, 5);
+    insertElem(&cld, 6);
+
+    display(cld, "Initial Dictionary");
+
+    deleteElem(&cld, 3);
+
+    display(cld, "Delete 3");
 }
