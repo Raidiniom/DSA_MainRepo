7aa599737a3ea63447a3254ec3add4e0c5e458ef
Raidiniom <raidiniom22ph@gmail.com>
2025-12-09 15:33:43 +0800

    Study?

--- Study/List/main.c
+++ Study/List/main.c
@@ -6,6 +6,17 @@ void main() {
 
     insertData(&list, createPerson("Jane", "Doe", 22));
     insertData(&list, createPerson("John", "Doe", 23));
+    insertData(&list, createPerson("Janell", "Doe", 24));
+    insertData(&list, createPerson("Joe", "Doe", 25));
+    insertData(&list, createPerson("Raine", "Doe", 26));
+    insertData(&list, createPerson("Voltaire", "Doe", 27));
+    insertData(&list, createPerson("Jack", "Doe", 28));
+    insertData(&list, createPerson("Jake", "Doe", 29));
+    insertData(&list, createPerson("Joan", "Doe", 30));
 
-    displayADTList(list);
+    displayADTList(list, "Initial List");
+
+    deleteData(&list, "Voltaire", "Doe");
+
+    displayADTList(list, "Delete One");
 }
